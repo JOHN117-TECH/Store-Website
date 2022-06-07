1bb9b3e0b8ed5a5a5d5d6511179a3159ad2d35dd
John Barreto <john.barreto@api-ux.com>
2022-06-07 15:20:14 -0500

    add funcionality on login-btn and styles when login-form is activated

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 let searchForm = document.querySelector('.header__search-form');
 let shoppingCart = document.querySelector('.header__shopping-cart');
+let loginForm = document.querySelector('.header__login-form');
 
 document.querySelector('#search-btn').onclick = () => {
   searchForm.classList.toggle('active');
@@ -10,4 +11,13 @@ document.querySelector('#search-btn').onclick = () => {
 
 document.querySelector('#cart-btn').onclick = () => {
   shoppingCart.classList.toggle('active');
+  searchForm.classList.remove('active');
+  loginForm.classList.remove('active');
+}
+
+document.querySelector('#login-btn').onclick = () => {
+  loginForm.classList.toggle('active');
+  shoppingCart.classList.remove('active');
+  searchForm.classList.remove('active');
+
 }
