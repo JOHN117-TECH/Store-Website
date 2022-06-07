6111a381c6e988eb7d0de3b6ace363e2c8b1f393
John Barreto <john.barreto@api-ux.com>
2022-06-07 18:02:27 -0500

    fix funcionaly btns

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,53 @@
 let searchForm = document.querySelector('.header__search-form');
 let shoppingCart = document.querySelector('.header__shopping-cart');
 let loginForm = document.querySelector('.header__login-form');
+let navbar = document.querySelector('.header__navbar');
+let buttons = document.querySelectorAll('.header__icons--item');
 
-document.querySelector('#search-btn').onclick = () => {
-  searchForm.classList.toggle('active');
-  shoppingCart.classList.remove('active');
-  loginForm.classList.remove('active');
-  navbar.classList.remove('active');
+function funcionalityBtns() {
+  for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', function () {
+      let currentBtn = document.querySelectorAll('.active-btn');
+      currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
+      this.className += ' active-btn';
+    })
+  }
+
+  document.querySelector('#search-btn').onclick = () => {
+    searchForm.classList.toggle('active');
+    shoppingCart.classList.remove('active');
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active');
+  }
+
+  document.querySelector('#cart-btn').onclick = () => {
+    shoppingCart.classList.toggle('active');
+    searchForm.classList.remove('active');
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active');
+  }
+
+  document.querySelector('#login-btn').onclick = () => {
+    loginForm.classList.toggle('active');
+    shoppingCart.classList.remove('active');
+    searchForm.classList.remove('active');
+    navbar.classList.remove('active');
+  }
+
+  document.querySelector('#menu-btn').onclick = () => {
+    navbar.classList.toggle('active');
+    shoppingCart.classList.remove('active');
+    searchForm.classList.remove('active');
+    loginForm.classList.remove('active');
+  }
+
+
+  window.onscroll = () => {
+    navbar.classList.remove('active');
+    shoppingCart.classList.remove('active');
+    searchForm.classList.remove('active');
+    loginForm.classList.remove('active');
+  }
 }
 
-document.querySelector('#cart-btn').onclick = () => {
-  shoppingCart.classList.toggle('active');
-  searchForm.classList.remove('active');
-  loginForm.classList.remove('active');
-}
-
-document.querySelector('#login-btn').onclick = () => {
-  loginForm.classList.toggle('active');
-  shoppingCart.classList.remove('active');
-  searchForm.classList.remove('active');
-
-}
+funcionalityBtns();
